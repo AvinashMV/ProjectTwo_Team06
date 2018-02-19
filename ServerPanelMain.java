fe2dbd727c0626f29124fd4feeb063818f0827c2
rishabmantri <rishab.mantri@gmail.com>
2018-02-19 16:20:54 -0700

    Update ServerPanelMain.java

--- ServerPanelMain.java
+++ ServerPanelMain.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 /**
  * Lab 2, Team 6
  * @author SER 516, Gary Morris (#78)
+ * @author SER 516, Rishab Mantri (#69)
  */
 public class ServerPanelMain extends JPanel {
     ServerPanelTop panelTop;
