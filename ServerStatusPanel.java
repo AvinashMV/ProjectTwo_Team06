a0630c1b3f15c85ed86417584656226d31e29c01
garv mathur <garv.mathur@gmail.com>
2018-02-19 23:21:52 -0700

    added server status panel

---
+++ ServerStatusPanel.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class ServerStatusPanel extends JPanel {
+	public ServerStatusPanel() {
+		 createLayout();
+	     createAndShowGUI();
+	}
+
+	private void createAndShowGUI() {
+		setBackground(Color.BLUE);
+		
+	}
+
+	private void createLayout() {
+		// TODO Auto-generated method stub
+		
+	}
+}
