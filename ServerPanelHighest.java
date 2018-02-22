3dd0560d20fcd307930946a152057459f13a0ab2
rishabmantri <rishab.mantri@gmail.com>
2018-02-22 00:33:18 -0700

    Update ServerPanelHighest.java

--- ServerPanelHighest.java
+++ ServerPanelHighest.java
@@ -7,30 +7,42 @@ import javax.swing.*;
 
 public class ServerPanelHighest extends JPanel{
 	
+    JPanel highestValuePanel;
+    JLabel highLabel;
+    JTextPane highTxt;
+    int highestValue;
+    
 	 public ServerPanelHighest() {
 	        createAndShowGUI();
 	    }
 	    
 	    private void createAndShowGUI() {
-	        JPanel test = new JPanel();
-	        JLabel highLabel = new JLabel("<html>Highest<br>Value</html>");
+	        highestValuePanel = new JPanel();
+	        highLabel = new JLabel("<html>Highest<br>Value</html>");
 	        
 	        highLabel.setFont(new Font("SansSerif", Font.BOLD, 15));
 	        highLabel.setSize(85, 60);
-	        test.setPreferredSize(new Dimension(120, 60));
-	        test.setBackground(StandardColor.LIGHT_BLUE);
-	        test.setBorder(BorderFactory.createLineBorder(Color.black));
+	        highestValuePanel.setPreferredSize(new Dimension(120, 60));
+	        highestValuePanel.setBackground(StandardColor.LIGHT_BLUE);
+	        highestValuePanel.setBorder(BorderFactory.createLineBorder(Color.black));
 	        
 	        //added text panel where the highest value will go.
-	        JTextPane highTxt = new JTextPane();
+	        highTxt = new JTextPane();
+                highTxt.setText("1024");
 	        highTxt.setBorder(BorderFactory.createLineBorder(Color.black));
 	        highTxt.setBackground(StandardColor.PINK);
 	        highTxt.setPreferredSize(new Dimension(120, 60));
-	        highTxt.setEditable(false);
+	        highTxt.setEditable(true);
 	        
-	        add(test);
-	        test.add(highLabel);
+	        add(highestValuePanel);
+	        highestValuePanel.add(highLabel);
 	        add(highTxt);
 	    }
+            
+                public int getHighestValue()
+                {
+                    highestValue= Integer.parseInt(highTxt.getText());
+                    return highestValue;
+                }
 
 }
