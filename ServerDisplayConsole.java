0ee5ada2a9072e2305edf4f9d984ab29a643cfad
Avinash MV <33912128+AvinashMV@users.noreply.github.com>
2018-02-22 14:26:33 -0700

    Update ServerDisplayConsole.java

--- ServerDisplayConsole.java
+++ ServerDisplayConsole.java
@@ -1,4 +1,4 @@
-//package clientServer.ProjectTwo_Team06;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -8,26 +8,53 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import javafx.scene.layout.Border;
+
+
 /**
  * For lab2, 516
  * @author avinash mathad vijayakumar
  *
  */
 
-public class ServerDisplayConsole extends JPanel{
+public class ServerDisplayConsole{
+	
+	private JPanel panel_console;
+	private JLabel label_console;
+	private JLabel label_msg;
+	
 	public ServerDisplayConsole() {
-		JLabel label=new JLabel("Console");
-		add(label);
+		ConstructPanel();
+	}
+	
+	public JPanel ConstructPanel() {
+		panel_console= new JPanel();
+		label_console=new JLabel("Console");	
+		panel_console.setLayout(new BorderLayout());
+		panel_console.add(label_console,BorderLayout.NORTH);
+		
+		label_msg=new JLabel("This is just text to display..");
+		panel_console.add(label_msg);
+		return panel_console;
+	}
+	
+	
+	public void SetServer_ConsoleData(String data) {
+		label_msg.setText(data);
 	}
 	
 	public static void main(String[] args) {
-		JPanel console=new ServerDisplayConsole();
+		ServerDisplayConsole ob=new ServerDisplayConsole();
+		ServerDisplayConsole ref_ob=new ServerDisplayConsole();
+		JPanel panel_to_add= ref_ob.ConstructPanel();
+		// to test the display in the console. 
+		String msg="ASU is No1 in US for innovation";
+		ref_ob.SetServer_ConsoleData(msg);
 		JFrame f=new JFrame("Simple Window");
-		f.setSize(800,800);
+		f.setSize(1900,1900);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.getContentPane().add(console);
+		f.getContentPane().add(panel_to_add);
 		f.pack();
-		//f.add(p);
 		f.setVisible(true);
 	}
 }
